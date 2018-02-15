bd148dc91755a49d6f3fc6a70d06145c1f7a87a3
Arcane-Panda <31314434+Arcane-Panda@users.noreply.github.com>
2018-02-15 12:05:11 -0700

    Update Creature.hpp

--- src/headers/Creature.hpp
+++ src/headers/Creature.hpp
@@ -9,51 +9,16 @@ class Creature : public Object {
 		vector<Item> inventory;
 	public:
 		Creature(string _name, unsigned _id, int _healthLevel, unsigned _healthMax, unsigned _damage)
-		: Object(_name, _id), healthLevel(_healthLevel), healthMax(_healthMax), damage(_damage)
-		{}
-
-		void setHealth(int _healthLevel, unsigned _healthMax) {
-			healthLevel = _healthLevel;
-			healthMax = _healthMax;
-		}
-
-		int getHealthLevel() {
-			return healthLevel;
-		}
-
-		unsigned getHealthMax() {
-			return healthMax;
-		}
-
-		void setDamage(unsigned _damage) {
-			damage = _damage;
-		}
-
-		unsigned getDamage() {
-			return damage;
-		}
-
-		void attack(Creature& victim) {
-			victim.healthLevel -= damage;
-		}
-
-		void enrage(float healthBuff, float damageBuff) {
-			healthLevel *= healthBuff;
-			healthMax *= healthBuff;
-			damage *= damageBuff;
-		}
-
-		void equip(Item newItem) {
-			inventory.push_back(newItem);
-			healthMax += newItem.getHealthBonus();
-			damage += newItem.getDamageBonus();
-		}
-
-		void unequip(Item newItem) {
-			inventory.erase(remove(inventory.begin(), inventory.end(), newItem), inventory.end());
-			healthMax -= newItem.getHealthBonus();
-			damage -= newItem.getDamageBonus();
-		}
+		: Object(_name, _id), healthLevel(_healthLevel), healthMax(_healthMax), damage(_damage);
+		void setHealth(int _healthLevel, unsigned _healthMax); 
+		int getHealthLevel(); 
+		unsigned getHealthMax(); 
+		void setDamage(unsigned _damage); 
+		unsigned getDamage();
+		void attack(Creature& victim);
+		void enrage(float healthBuff, float damageBuff);
+		void equip(Item newItem); 
+		void unequip(Item newItem); 
 };
 
 #endif
